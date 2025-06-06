903af2be2baeb1a744ec46565545fdd5b508fd11
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2025-06-06 12:12:57 -0400

    wip testing

--- src/fs_log_init.c
+++ src/fs_log_init.c
@@ -34,35 +34,53 @@ static struct fs_mount_t mp = {
   .mnt_point = DISK_MOUNT_PT,
 };
 
+static void prv_ioctl_test(void) {
+  do {
+    static const char *disk_pdrv = DISK_DRIVE_NAME;
+    uint64_t memory_size_mb;
+    uint32_t block_count;
+    uint32_t block_size;
+
+    if (disk_access_ioctl(disk_pdrv, DISK_IOCTL_CTRL_INIT, NULL) != 0) {
+      LOG_ERR("Storage init ERROR!");
+      break;
+    }
+
+    if (disk_access_ioctl(disk_pdrv, DISK_IOCTL_GET_SECTOR_COUNT, &block_count)) {
+      LOG_ERR("Unable to get sector count");
+      break;
+    }
+    LOG_INF("Block count %u", block_count);
+
+    if (disk_access_ioctl(disk_pdrv, DISK_IOCTL_GET_SECTOR_SIZE, &block_size)) {
+      LOG_ERR("Unable to get sector size");
+      break;
+    }
+    printk("Sector size %u\n", block_size);
+
+    memory_size_mb = (uint64_t)block_count * block_size;
+    printk("Memory Size(MB) %u\n", (uint32_t)(memory_size_mb >> 20));
+
+    if (disk_access_ioctl(disk_pdrv, DISK_IOCTL_CTRL_DEINIT, NULL) != 0) {
+      LOG_ERR("Storage deinit ERROR!");
+      break;
+    }
+  } while (0);
+}
+
 int fs_log_init(void) {
+  prv_ioctl_test();
+
+  LOG_INF("Mounting disk at '%s'", DISK_MOUNT_PT);
   int res = fs_mount(&mp);
 
   if (res == 0) {
-    printk("Disk mounted.\n");
-    /* Try to unmount and remount the disk */
-    res = fs_unmount(&mp);
-    if (res != 0) {
-      printk("Error unmounting disk\n");
-      return res;
-    }
-    res = fs_mount(&mp);
-    if (res != 0) {
-      printk("Error remounting disk\n");
-      return res;
-    }
-
-    if (lsdir(disk_mount_pt) == 0) {
-      printk("Directory listing successful.\n");
-    } else {
-      printk("Error listing directory.\n");
-    }
-  } else {
-    printk("Error mounting disk.\n");
+    LOG_INF("Disk mounted.\n");
   }
 
-  fs_unmount(&mp);
+  // fs_unmount(&mp);
 
-  return res;
+  return 0;  // res;
 }
 
 #endif  // CONFIG_FILE_SYSTEM
