8de6afe2c98eda9b4b6861e8e9197e583189392c
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2026-01-22 11:47:13 -0500

    Example of runtime controlled periodic upload

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/shell/shell.h>
 
 #include "app_version.h"
 #include "memfault/components.h"
@@ -18,13 +19,31 @@ extern const char* memfault_zephyr_get_device_id(void);
 void memfault_platform_get_device_info(sMemfaultDeviceInfo* info) {
   // override the software version only to demonstrate inserting the GIT SHA
   *info = (sMemfaultDeviceInfo){
-    .device_serial = memfault_zephyr_get_device_id(),
-    .software_type = CONFIG_MEMFAULT_BUILTIN_DEVICE_INFO_SOFTWARE_TYPE,
-    .software_version =
-      CONFIG_MEMFAULT_BUILTIN_DEVICE_INFO_SOFTWARE_VERSION "+" ZEPHYR_MEMFAULT_EXAMPLE_GIT_SHA1,
-    .hardware_version = CONFIG_MEMFAULT_BUILTIN_DEVICE_INFO_HARDWARE_VERSION,
+      .device_serial = memfault_zephyr_get_device_id(),
+      .software_type = CONFIG_MEMFAULT_BUILTIN_DEVICE_INFO_SOFTWARE_TYPE,
+      .software_version = CONFIG_MEMFAULT_BUILTIN_DEVICE_INFO_SOFTWARE_VERSION
+      "+" ZEPHYR_MEMFAULT_EXAMPLE_GIT_SHA1,
+      .hardware_version = CONFIG_MEMFAULT_BUILTIN_DEVICE_INFO_HARDWARE_VERSION,
   };
 }
+
+// Basic shell command to call memfault_zephyr_port_http_periodic_upload()
+static int cmd_memfault_upload(const struct shell* shell, size_t argc,
+                               char** argv) {
+  // requires 1 arg, 1 or 0, to enable/disable
+  if (argc == 2) {
+    bool enable = strncmp(argv[1], "1", 2) == 0;
+    memfault_zephyr_port_http_periodic_upload_enable(enable);
+    shell_print(shell, "Periodic upload %s", enable ? "enabled" : "disabled");
+    return 0;
+  }
+
+  shell_print(shell, "Usage: memfault_upload <1|0>");
+  return 1;
+}
+
+SHELL_CMD_REGISTER(memfault_upload, NULL, "Trigger Memfault data upload",
+                   cmd_memfault_upload);
 
 int main(void) {
   LOG_INF("Memfault Demo App! Board %s\n", CONFIG_BOARD);
