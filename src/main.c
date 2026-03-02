076cadf0e630aa771b3b2e9effdba5b2d91931bd
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2026-03-01 20:04:43 -0500

    Enable wifi auto-connect
    Settings for saving wifi creds, enable auto-connect.
    
    Also fix TLS on Zephyr v4.3.0 at least (PSA might work better on v4.4
    dev?), by disabling PSA.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,12 @@
 //! @file
 
+#include <stdio.h>
 #include <string.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/net/net_if.h>
+#include <zephyr/net/net_mgmt.h>
+#include <zephyr/net/wifi_mgmt.h>
 
 #include "app_version.h"
 #include "memfault/components.h"
@@ -15,14 +19,30 @@ LOG_MODULE_REGISTER(mflt_app, LOG_LEVEL_DBG);
 // Memfault SDK release
 extern const char* memfault_zephyr_get_device_id(void);
 
+static const char* prv_get_software_version(void) {
+  static char s_software_version[64];
+
+  // CI builds will often set git sha = "". in that case, omit that portion of
+  // the software version string
+  if (ZEPHYR_MEMFAULT_EXAMPLE_GIT_SHA1[0] == '\0') {
+    return CONFIG_MEMFAULT_BUILTIN_DEVICE_INFO_SOFTWARE_VERSION;
+  }
+
+  // Otherwise, append it to demonstrate how to include git sha in the software
+  // version string
+  snprintf(s_software_version, sizeof(s_software_version), "%s+%s",
+           CONFIG_MEMFAULT_BUILTIN_DEVICE_INFO_SOFTWARE_VERSION,
+           ZEPHYR_MEMFAULT_EXAMPLE_GIT_SHA1);
+  return s_software_version;
+}
+
 void memfault_platform_get_device_info(sMemfaultDeviceInfo* info) {
   // override the software version only to demonstrate inserting the GIT SHA
   *info = (sMemfaultDeviceInfo){
-    .device_serial = memfault_zephyr_get_device_id(),
-    .software_type = CONFIG_MEMFAULT_BUILTIN_DEVICE_INFO_SOFTWARE_TYPE,
-    .software_version =
-      CONFIG_MEMFAULT_BUILTIN_DEVICE_INFO_SOFTWARE_VERSION "+" ZEPHYR_MEMFAULT_EXAMPLE_GIT_SHA1,
-    .hardware_version = CONFIG_MEMFAULT_BUILTIN_DEVICE_INFO_HARDWARE_VERSION,
+      .device_serial = memfault_zephyr_get_device_id(),
+      .software_type = CONFIG_MEMFAULT_BUILTIN_DEVICE_INFO_SOFTWARE_TYPE,
+      .software_version = prv_get_software_version(),
+      .hardware_version = CONFIG_MEMFAULT_BUILTIN_DEVICE_INFO_HARDWARE_VERSION,
   };
 }
 
@@ -33,6 +53,15 @@ int main(void) {
 
   memfault_device_info_dump();
   memfault_zephyr_port_install_root_certs();
+  // Initiate auto connection on the wifi interface
+  struct net_if* iface = net_if_get_default();
+  int rc = net_mgmt(NET_REQUEST_WIFI_CONNECT_STORED, iface, NULL, 0);
+
+  if (rc) {
+    printk(
+        "an error occurred when trying to auto-connect to a network. err: %d",
+        rc);
+  }
 
   return 0;
 }
